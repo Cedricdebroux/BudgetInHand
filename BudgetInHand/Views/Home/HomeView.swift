1e0e68cf0f774149bbe616441f17b7a6cba20270
Cedric Debroux <cedric@debroux.ovh>
2022-12-08 09:38:39 +0100

    Feat/import feature charts

--- BudgetInHand/Views/Home/HomeView.swift
+++ BudgetInHand/Views/Home/HomeView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct HomeView: View {
+    var valuesPie : [Double] = [300,400,600,500]
+    var namePie : [String] = ["Carburant","Energie","Comissions","Charges"]
+    var colorChart: [Color] = [Color.fromInts(r: 0, g: 181, b: 216), Color.fromInts(r: 0, g: 119, b: 182),Color.fromInts(r: 144, g: 224, b: 238),Color.fromInts(r: 3, g: 4, b: 94)]
+    let imageArray = ["car.fill","bolt.fill","cart.fill","house.fill"]
+    //let imageView = UIImageView(image: image)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+     
+            VStack{
+                
+                VStack{
+                    PieChartView(
+                        values: valuesPie,
+                        names: namePie,
+                        formatter: { $0.description },
+                        colors: colorChart,
+                        iconNames: imageArray,
+                        backgroundColor: Color.fromInts(r: 250,
+                                                        g: 250,
+                                                        b: 250))
+                }
+                Spacer()
+                    .frame(height: 180)
+                
+                NavigationStack{
+                    List {
+                        ForEach(0..<4) { _ in
+                            Text("Integer")
+                        }
+                    }
+                }
+                
+            }
+        }
 }
 
 struct HomeView_Previews: PreviewProvider {
