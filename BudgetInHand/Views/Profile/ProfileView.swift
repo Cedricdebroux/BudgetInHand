03c4943e1e6c5668373640439827eebd4e99afa9
Cedric Debroux <cedric@debroux.ovh>
2022-12-13 15:05:33 +0100

    Feat/settingsView and loginView complete

--- BudgetInHand/Views/Profile/ProfileView.swift
+++ BudgetInHand/Views/Profile/ProfileView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ProfileView: View {
     @EnvironmentObject var appModel: BudgetInHandModel
+    @State private var showAlert = false
     var body: some View {
         NavigationView{
             VStack{
@@ -29,26 +30,46 @@ struct ProfileView: View {
                     
                     NavigationLink(destination: Text("Paramètres du compte")) {
                         Label("Paramètres du compte", systemImage: "gear")
+                            .foregroundColor(Color("Blue600"))
                     }
-                    NavigationLink(destination: Text("Changer le mot de passe")) {
-                        Text("Changer le mot de passe")
+                    NavigationLink(destination: Text("Budget global")) {
+                        Label("Définisez votre budget", systemImage: "eurosign.circle")
+                            .foregroundColor(Color("Blue600"))
                     }
-                    NavigationLink(destination: Text("Relevés de compte")) {
-                        Text("Relevés de compte")
+                    NavigationLink(destination: Text("Se désinscrire")
+                    ) {
+                        Label("Changer votre mot de passe", systemImage: "person.badge.key")
+                            .foregroundColor(Color("Blue600"))
+                    }
+
+                    NavigationLink(destination: Text("relevés de compte")
+                    ) {
+                        Label("Relevés de compte", systemImage: "list.bullet.rectangle.portrait")
+                            .foregroundColor(Color("Blue600"))
                     }
         
                     
-                    NavigationLink(destination: Text("Se désinscrire")
-                    ) {
-                        Label("Se désinscrire", systemImage: "wrongwaysign")
-                            .foregroundColor(Color.red)
+                    Button{
+                        showAlert = true} label: {
+                            Label("Se désinscrire", systemImage: "wrongwaysign")
+                                .foregroundColor(Color.red)
+                        }
+                    
+                }
+                .alert("Attention, la suppression de votre compte sera définitive. Êtes vous certain de vouloir supprimer votre compte ?",isPresented: $showAlert){
+                        Button("Oui", role: .cancel){
+                            
+                        }
+                        Button("Non", role: .none){
+                            
+                        }
                     }
                 }
                 
             }
         }
     }
-}
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
--- BudgetInHand/Views/Settings/SettingsView.swift
+++ BudgetInHand/Views/Settings/SettingsView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Settings view")
+        VStack{
+            Label("Paramètres du compte", systemImage: "gear")
+                .font(.title2)
+                .foregroundColor(Color("Blue600"))
+            NavigationView{
+                List {
+                    NavigationLink(destination: Text("Endroits préférés")) {
+                        Label("Endroits préférés", systemImage: "mappin.and.ellipse")
+                            .foregroundColor(Color("Blue600"))
+                    }
+                    NavigationLink(destination: Text("Autorisations")) {
+                        Label("Autorisations", systemImage: "key.viewfinder")
+                            .foregroundColor(Color("Blue600"))
+                    }
+                    NavigationLink(destination: Text("Durée du budget")) {
+                        Label("Durée du budget", systemImage: "timer")
+                            .foregroundColor(Color("Blue600"))
+                    }
+                }
+            }
+        }
     }
 }
 
