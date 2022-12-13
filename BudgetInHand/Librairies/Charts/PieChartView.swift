16cf8e8aa91f2ea652f62044dacb05a1fc464eba
Cedric Debroux <cedric@debroux.ovh>
2022-12-13 08:06:29 +0100

    Feat/complete before merge

--- BudgetInHand/Librairies/Charts/PieChartView.swift
+++ BudgetInHand/Librairies/Charts/PieChartView.swift
@@ -58,8 +58,8 @@ public struct PieChartView: View {
                 ZStack{
                     ForEach(0..<self.values.count, id: \.self){ i in
                         PieSlice(pieSliceData: self.slices[i], angleSpace: angleSpace)
-//                            .scaleEffect(self.activeIndex == i ? 1.03 : 1)
-//                            .animation(Animation.spring())
+                            .scaleEffect(self.activeIndex == i ? 1.03 : 1)
+                            .animation(Animation.spring())
                     }
                     .frame(width: widthFraction * geometry.size.width, height: widthFraction * geometry.size.width)
                     .gesture(
