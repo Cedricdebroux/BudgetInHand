2989fd9b75e275435ab0a02f3f7a6282f2650c20
Cedric Debroux <cedric@debroux.ovh>
2022-12-13 10:13:51 +0100

    Feat/Page Profile Wip

--- BudgetInHand/Views/Profile/ProfileView.swift
+++ BudgetInHand/Views/Profile/ProfileView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile view")
+        NavigationView{
+            VStack{
+                Spacer()
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                HStack{
+                    Text("Prénom")
+                    Text("Nom")
+                }
+                List {
+                    NavigationLink(destination: Text("Paramètre du compte"), label: {
+                        
+                    })
+                }
+            }
+        }
     }
 }
 
